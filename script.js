6fab8d101d23caeb1ca5d870a66018c1f742dd18
Trevor Kirpaul <30737677+trevorkirpaul@users.noreply.github.com>
2017-10-09 08:38:00 -0400

    Added much more functionality and removed FA library
    -Edit Task functionality implemented
    -Task Description functionality added
    -Modal added
    -Font Awesome local folder removed and replaced with CDN link

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 var $task = document.getElementById('inpTaskName').value; //placeholder for task name 
+var $editPane = document.getElementById('editModal');
+
+var $btnModalConfirm = document.getElementById('btnEditConfirm'); //edit modal confirm button
+var $btnModalCancel = document.getElementById('btnEditCancel'); //edit modal cancel button
+var $modalTaskName = document.getElementById('inpTaskNameEdit'); //edit modal name input field
+var $modalTaskDes = document.getElementById('inputTaskDesEdit'); // edit modal description input field
+
+//declare vars to hold the DOM placement.
+//I'll refer to these when I'm editting
+//the dynamic tasks using a static modal
+
+var $$holdTaskTitle;
+var $$holdTaskDes;
 
 
 function addTask() {
@@ -19,7 +32,9 @@ function addTask() {
 
 	var $divOne = document.createElement("div"); //create div with task name and description
 
-	var $taskTitle = document.createElement("h2"); 
+	var $taskTitle = document.createElement("h2"); //create h2 task title
+
+	var $taskDes = document.createElement("p"); //create p ele for task description, initally blank
 
 	$taskTitle.appendChild(document.createTextNode($task));
 
@@ -82,17 +97,45 @@ function addTask() {
 
 	});
 
-	//edit task
+
+	
+
+
+
+
+	//edit task button functionality
 
 	$iPencil.addEventListener('click', function(){
+		
+		$editPane.style.display = 'block';
 
-		alert('edit');
+		//locate current task name
+		var $currentTaskTitle = this.parentElement.previousSibling.firstChild.innerHTML;
+		var $currentTaskDes = this.parentElement.previousSibling.firstChild.nextSibling.innerHTML;
+
+		//set initial value for modal input title and des field to current task title b4 edit
+		$modalTaskName.value = $currentTaskTitle;
+		$modalTaskDes.value =  $currentTaskDes;
+
+
+		//hold the DOM location for this current task so I can edit with static modal
+		$$holdTaskTitle = this.parentElement.previousSibling.firstChild;
+		$$holdTaskDes = this.parentElement.previousSibling.firstChild.nextSibling;
+
+
+
 
 	});
 
 
 
+
+
+
+
+
 	$divOne.appendChild($taskTitle); 	//attach h2 to divOne
+	$divOne.appendChild($taskDes); //attach task description p element to divOne
 	$sectionTaskGrid.appendChild($divOne); //attach first div to #taskGrid
 	$sectionTaskGrid.appendChild($divButtonsTaskPane); //attach second div to #taskGrid
 	$divTask.appendChild($sectionTaskGrid); //attach #taskGrid to div#task
@@ -109,15 +152,63 @@ function addTask() {
 
 
 
-//fxn to add task
-document.getElementById('btnTaskEnter').onclick = function(){
+	//fxn to add task
+	document.getElementById('btnTaskEnter').onclick = function(){
 
-	var $input = document.getElementById('inpTaskName');
+		var $input = document.getElementById('inpTaskName');
 
-	if ($input.value != "") {
-		addTask();
+		if ($input.value != "") {
+			addTask();
+			$input.value = "";
+		}
 	}
+
+	//fxn for each ban button to remove parent task
+
+
+
+	// modal function
+
+	var $closeModal = document.getElementById('btnClose'); // button to close modal
+
+	$closeModal.onclick = function() {
+
+		$editPane.style.display = 'none';
+	}
+
+	//modal confirm task edit
+
+
+
+	$btnModalConfirm.onclick = function() {
+
+
+
+		$$holdTaskTitle.innerHTML = $modalTaskName.value;
+
+		$$holdTaskDes.innerHTML = $modalTaskDes.value;
+
+
+
+
+		$editPane.style.display = 'none';
+
+		$modalTaskName.value = "";
+		$modalTaskDes.value = "";
+
+		$$holdTaskTitle = "";
+		$$holdTaskDes = "";
+
+	}
+
+//modal cancel button fxn
+
+$btnModalCancel.onclick = function() {
+
+	$$holdTaskTitle = "";
+	$$holdTaskDes = "";
+	$modalTaskName.value = "";
+	$modalTaskDes.value = "";
+	$editPane.style.display = 'none';
+
 }
-
-//fxn for each ban button to remove parent task
-
